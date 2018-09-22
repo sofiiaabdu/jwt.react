b16e3a74e0af1d4274f7feb9bfe2099de9780df9
sofiiaabdu <sofiiaa@tahometer.com>
2018-09-22 18:08:29 +0300

    Created the project, Login form.

---
+++ src/Login.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            password: '',
+        };
+
+        this.handlePassChange = this.handlePassChange.bind(this);
+        this.handleUserChange = this.handleUserChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(evt) {
+        evt.preventDefault();
+
+
+    }
+
+    handleUserChange(evt) {
+        this.setState({
+            username: evt.target.value,
+        });
+    };
+
+    handlePassChange(evt) {
+        this.setState({
+            password: evt.target.value,
+        });
+    }
+
+    render() {
+        return (
+            <div className="Login">
+                <form onSubmit={this.handleSubmit}>
+                    <label>User Name</label>
+                    <input type="text" data-test="username" value={this.state.username} onChange={this.handleUserChange} />
+
+                    <label>Password</label>
+                    <input type="password" data-test="password" value={this.state.password} onChange={this.handlePassChange} />
+
+                    <input type="submit" value="Log In" data-test="submit" />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Login
